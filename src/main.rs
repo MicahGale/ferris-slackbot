0ec89675e7da5c6051428b68d61b130628e9ba8b
Micah Gale <mg@gale.engineer>
2024-01-25 21:22:31 -0600

    Added tool to read the codes of line.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::process::Command;
 
 fn main() {
     let version = get_compiler_version();
     let binary_version = convert_text_to_binary(&version);
+    // TODO: take date from command line and get number of days.
+    let i = 1;
+    let code_line = get_line_of_source(i);
+    println!("{}\n{}", binary_version, code_line);
 }
 
 fn get_compiler_version() -> String {
@@ -22,4 +28,24 @@ fn convert_text_to_binary(message: &String) -> String {
     }
     ret = ret.replace("1", "🦀").replace("0", "🦝");
     ret
+}
+
+fn get_line_of_source(i: usize) -> String {
+    let filename = "src/main.rs";
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+    let mut num_lines = 0;
+    for (index, _) in reader.lines().enumerate() {
+        num_lines = index
+    }
+
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    for (index, line) in reader.lines().enumerate() {
+        if i.rem_euclid(num_lines) == index {
+            return line.unwrap();
+        }
+    }
+    "".to_string()
 }
